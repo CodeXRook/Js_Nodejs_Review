eedfe29212f7945099b330ed75a1c8cd3b5595f4
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-18 23:40:01 -0400

    console log, see foo

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 //Javascript is a non static type language
 //Complex: Array. Prim:numbers  'strings'  boolean,   complex: objects
-           const fooArray = [      1,  2,    'foo', 'bar', true, false, {foo: 1, bar: 2}];
+           let fooArray = [      1,  2,    'foo', 'bar', true, false, {foo: 1, bar: 2}];
            fooArray = ['foo', 'bar', 'goo'];
+           
+           console.log(fooArray);
