4e82208d5b1463c13d6a1c2d578983b2c947561f
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-19 19:10:10 -0400

    class type

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
-//Javascript is a non static type language
-//Complex: Array. Prim:numbers  'strings'  boolean,   complex: objects
-           let fooArray = [      1,  2,    'foo', 'bar', true, false, {foo: 1, bar: 2}];
-           fooArray = ['foo', 'bar', 'goo'];
+// //Javascript is a non static type language
+// //Complex: Array. Prim:numbers  'strings'  boolean,   complex: objects
+//            let fooArray = [      1,  2,    'foo', 'bar', true, false, {foo: 1, bar: 2}];
+//            fooArray = ['foo', 'bar', 'goo'];
            
-           console.log(fooArray);
+//            console.log(fooArray);
 
-           // if ( undefined) {console.log('hi');} else { console.log('bye');}
+//            // if ( undefined) {console.log('hi');} else { console.log('bye');}
 
-           const runLoop = (paramOne, paramTwo) => {
-            for ( let i=0; i<10; i++) {
-                //Condition
-                //=== is strickly the same type exactly 
-                //== check for the same type regargless act as a conditional value 
-                if(i === 5) {
-                    console.log('i is 5!!!');
-                }
+//            const runLoop = (paramOne, paramTwo) => {
+//             for ( let i=0; i<10; i++) {
+//                 //Condition
+//                 //=== is strickly the same type exactly 
+//                 //== check for the same type regargless act as a conditional value 
+//                 if(i === 5) {
+//                     console.log('i is 5!!!');
+//                 }
 
-               console.log(i);
-             }
+//                console.log(i);
+//              }
 
-             console.log(paramOne, paramTwo);
-             paramTwo();
-             
-           }
+//              console.log(paramOne, paramTwo);
+//              paramTwo();
 
-           const logBam = () => console.log('bam');
+//            }
 
-           runLoop('Zoo', logBam);
+//            const logBam = () => console.log('bam');
+
+//            runLoop('Zoo', logBam);
+
+
+class Lion {
+    constructor(){
+        
+    }
+}
